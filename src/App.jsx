1977a71f906cb665bded95dbcb5da531b3b9971e
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-24 18:48:42 +0800

    add input and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import "./App.css";
+import TaskForm from "./components/TaskForm";
 
 const App = () => {
   return (
     <div className="app">
-      <header className="app-header">Header Section</header>
+      <TaskForm />
       <main className="app_main">
         <section className="task_column">Section 1</section>
         <section className="task_column">Section 2</section>
