1a4cf1bb1cfcf25dce961dc147b185b881e8d82f
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-25 11:33:00 +0800

    finish task column component title and icons

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Todo from "../assets/direct-hit.png";
 
-const TaskColumn = () => {
+import "./TaskColumn.css";
+
+const TaskColumn = ({ title, icon }) => {
   return (
     <section className="task_column">
-      <h2>To do </h2>
+      <h2 className="task_column_heading">
+        <img className="task_column_icon" src={icon} alt="" />
+        {title}
+      </h2>
     </section>
   );
 };
